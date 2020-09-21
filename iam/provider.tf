43515e9684c05efbee130e2b4637e61b6f7af042
k3forx <kanata.miyahana@bebit.com>
2020-09-21 17:06:36 +0900

    change the provider

--- iam/provider.tf
+++ iam/provider.tf
@@ -1,6 +1,11 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
+  # shared_credentials_file = "~/.aws/credentials"
+  # profile                 = "admin-kanata"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region                  = "ap-northeast-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "admin-kanata"
   version                 = "2.62"
 }
