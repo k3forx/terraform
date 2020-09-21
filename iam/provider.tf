8236fc22df14fe0a279686146c326af3948125ad
k3forx <kanata.miyahana@bebit.com>
2020-09-21 17:18:06 +0900

    modify provider.tf

--- iam/provider.tf
+++ iam/provider.tf
@@ -1,8 +1,6 @@
 provider "aws" {
-  # shared_credentials_file = "~/.aws/credentials"
-  # profile                 = "admin-kanata"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region                  = "ap-northeast-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "admin-kanata"
   version                 = "2.62"
 }
